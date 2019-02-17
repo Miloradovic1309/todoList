a589e4d459907355064c9ed56fc7c3670a2c5850
Danijel <you@example.com>
2019-02-17 17:37:40 +0100

    update database

--- models/index.js
+++ models/index.js
@@ -1,7 +1,14 @@
 var mongoose = require("mongoose");
 mongoose.set('debug', true);
+
+
+var databaseUrl = process.env.DATABASEURL || 'mongodb://localhost/todos_api';
+
+mongoose.connect(databaseUrl, { useNewUrlParser: true } );
 //mongoose.connect('mongodb://localhost/todos_api', { useNewUrlParser: true } );
-mongoose.connect('mongodb://Danijel:rusty1991@ds239055.mlab.com:39055/todolist', { useNewUrlParser: true } );
+
+//mongoose.connect('mongodb://<name>:<password>.mlab.com:39055/todolist', { useNewUrlParser: true } );
+
 //mongodb://Danijel:rusty1991@ds239055.mlab.com:39055/todolist
 //mongoose.connect('mongodb://localhost:27017/todos_api', { useNewUrlParser: true });
 //mongoose.connect('mongodb:// 127.0.0.1:27017/todos_api', { useNewUrlParser: true });
