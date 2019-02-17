8fb99bfeb75a5d0cc50916911ebbf4b9badbb7f6
Danijel <you@example.com>
2019-02-17 10:53:47 +0100

    add mongolab

--- models/index.js
+++ models/index.js
@@ -1,6 +1,8 @@
 var mongoose = require("mongoose");
 mongoose.set('debug', true);
-mongoose.connect('mongodb://localhost/todos_api', { useNewUrlParser: true } );
+//mongoose.connect('mongodb://localhost/todos_api', { useNewUrlParser: true } );
+mongoose.connect('mongodb://Danijel:rusty1991@ds239055.mlab.com:39055/todolist', { useNewUrlParser: true } );
+//mongodb://Danijel:rusty1991@ds239055.mlab.com:39055/todolist
 //mongoose.connect('mongodb://localhost:27017/todos_api', { useNewUrlParser: true });
 //mongoose.connect('mongodb:// 127.0.0.1:27017/todos_api', { useNewUrlParser: true });
 
